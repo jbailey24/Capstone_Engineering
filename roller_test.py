11f01818418a37ad85b54eeb9d0d8b1f3fbd8d33
jbailey24 <56133021+jbailey24@users.noreply.github.com>
2022-01-19 14:49:04 -0500

    Update roller_test.py

--- roller_test.py
+++ roller_test.py
@@ -13,11 +13,17 @@ GPIO.setmode(GPIO.BCM)
 # Use built-in internal pullup resistor so the pin is not floating
 # if using a momentary push button without a resistor.
 GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+switch_state = 0
+previous = 0
 
 while True:
         if GPIO.input(switch) == False:
-                print("on")
+                switch_state = 1
 
         if GPIO.input(switch) == True:
-                print("off")
-        time.sleep(.1)
+                switch_state = 0
+
+        if switch_state == 1 and switch_state != previous:
+                print('on')
+
+        previous = switch_state
